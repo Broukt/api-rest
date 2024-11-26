0acb7835e0a54c045be675a91d3f4e49ee9f997d
Jairo Calcina <j.calcina.v@gmail.com>
2024-11-26 23:17:28 +0000

    feat: add routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const {
+    createUserValidation,
+    getUserValidation,
+    getUsersValidation,
+    updateUserValidation,
+    deleteUserValidation
+} = require('../helpers/validator');
+const validateFields = require('../middleware/validateFields');
+
+router.post('/', createUserValidation, validateFields, userController.createUser);
+router.get('/:uuid', getUserValidation, validateFields, userController.getUser);
+router.get('/', getUsersValidation, validateFields, userController.getUsers);
+router.patch('/:uuid', updateUserValidation, validateFields, userController.updateUser);
+router.delete('/:uuid', deleteUserValidation, validateFields, userController.deleteUser);
